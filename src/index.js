71d0d4c43abc1e15b9dd8ecd34d109bb8b9177b4
Alexandre Masy <alexandre@infinity-mobile.io>
2021-06-10 05:26:24 +0200

    Version 0.0.12 with 1 changes. - icon `debug` updated

--- src/index.js
+++ src/index.js
@@ -1,12 +1,1 @@
-import { version } from '../package.json'
-const path = 'https://cdn.sayl.cloud/spices/icons/'
-const sprite = ['spices-icons.svg', version].join('?v=')
-const json = ['spices-icons.json', version].join('?v=')
-const meta = {
-  json: [path, json].join(''),
-  sprite: [path, sprite].join(''),
-  version
-}
-
-export { default as install } from './install'
-export { meta }
+export { default as SpicesCloveRegular } from './spices-clove-regular/manifest.json'
